c923e862a8e47399a6e10179811cba8dfa15eefa
Germán Cruz <gdcruzgarma@gmail.com>
2020-03-22 23:41:05 -0600

    Inicio del proyecto PeSD SSPCAM, creación del proyecto y primer modelo Disability (tipo de discapacidad)

---
+++ PeSD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PeSD.MvcApplication" Language="C#" %>
---
+++ PeSD/Models/Disability.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PeSD.Models
+{
+    public class Disability
+    {
+        [Key]
+        public int DisabilityId { get; set; }
+
+        [Required(ErrorMessage ="El nombre de la discapacidad es requerida")]
+        [MaxLength(50)]
+        public String DisabilityName { get; set; }
+
+    }
+}
